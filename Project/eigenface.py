f3555064542ec87af4c9a80200f88fdf99cedb04
cdilga <c.dilger1@gmail.com>
2018-11-20 09:51:31 -0500

    Refactored eigenfaces to use dependency injection

--- Project/eigenface.py
+++ Project/eigenface.py
@@ -19,6 +19,8 @@ from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 
 import pickle
 
+import eigenfaces
+
 image_x = image_y = 32
 
 verbose = 5
@@ -46,120 +48,27 @@ def showImage(image):
 def pltImage(image):
     plt.imshow(np.resize(image, (image_x, image_y)), cmap=plt.cm.gray)
     plt.show()
+#########################################################################
+#
+#   Real Eigenfaces classifier, using some models
+#   
+#   Uses all the images in the set for training, however the original
+#   paper found that the best results were not always had with all 
+#   images in the training set
+#
+#########################################################################
 
 
-#train a model
-
-#produce results
-
-#train another model
-
-#produce results
-
-#compare all results
-
-'''
-images = loadImages(r'C:/Users/cdilg/Documents/NEU/DS4400/Project/croppedyale/yaleB01')
-
-mean_face = np.mean(images, axis = 0)
-
-rpca = PCA(100, svd_solver='randomized')
-rpca.fit(images - mean_face)
-
-pca_images = rpca.transform(images - mean_face)
-
-#pltImage(rpca.components_[0,:])
-'''
-
-
-
-'''
-In the style of the original paper we will define a distance metric, 
-below a cutoff value will classify a given source image as a face. 
-This is in effect a binary classifier, and will tell us if what we're looking at is a face
-
-In the style of the paper, we project the new image into the face space, and then compute euclidean distance
-
-
-We use the initial set of 65 images, and then a mix of the second folder images and Caltech101 images of not faces
-
-First we just get the distance metrics from the 'face' to it's face space. If it's a long distance, we haven't 
-captured the variance well.
-
-Similarly, we can use existing training examples in a similar way to a k-nn and classify. We'd need a k-nn modified
-to allow a cutoff for an 'out of class' value, which we can implement.
-
-
-'''
-
-def projectFaceView(face, pca, mean_face):
-    face = np.array(face)
-    mean_face = np.array(face)
-    normal_face = face - mean_face
-    
-    pca_face = pca.transform(normal_face.reshape(1, -1))
-    projected_face = np.matmul(pca_face, pca.components_)
-    pltImage(projected_face)
-
-def eigenFaceView(pca):
-    eigFaces = pca.components_.reshape((len(pca.components_), image_x, image_y))
-    pltImage(eigFaces[0])
-
-def faceSolver(face):
-    pca_face = rpca.transform(face.reshape(1, -1) - mean_face)
-
-    #this step is rather important - we actually obtain the orig face back, but it's actually
-    #just a linear combination of eigenvectors
-    
-    projected_face = np.matmul(pca_face, rpca.components_)
-
-    #NOTE confusion around how to normalise new inputs, do we divide by the average of a class,
-    # or all classes, or by the mean of the data itself?
-    diff = face - projected_face
-
-    return np.linalg.norm(diff)
-
-def calculateDistance(imageset):
-
-    #for training data
-    distances = np.apply_along_axis(faceSolver, axis = 1, arr=imageset)
-    #print(distances)
-    print(scipy.stats.describe(distances))
-    print(scipy.stats.tstd(distances))
-    #for new images
-    return distances
-
-def toyDistanceClassifier():
-    print('Testing images distance metric')
-    calculateDistance(images)
-
-    print('Non face images distance metric')
-    not_faces = loadImages(
-        r'C:/Users/cdilg/Documents/NEU/DS4400/Project/not-faces', '.jpg')[0:65,:]
-    not_face_dist = calculateDistance(not_faces)
-
-    print('Testing face images')
-    unseen_images = loadImages(
-        r'C:/Users/cdilg/Documents/NEU/DS4400/Project/croppedyale/yaleB02')
-    test_face_dist = calculateDistance(unseen_images)
-    #Define the distance cutoff to be a value, 34000 chosen after observing the accuracy
-
-    threshold = 41000
-
-    fp = np.sum(not_face_dist < threshold)
-    tn = not_face_dist.size - fp
-    tp = np.sum(test_face_dist < threshold)
-    fn = test_face_dist.size - tp
-
+def prae(confusionMat):
+    tn, fp, fn, tp = confusionMat.ravel()
     acc = (tp+tn)/(not_face_dist.size + test_face_dist.size)
     err = 1 - acc
-    print('Precision: {}'.format(tp/(tp+fp)))
-    print('Recall: {}'.format(tp/(tp+fn)))
-    print('Accuracy: {}'.format(acc))
-    print('Error: {}'.format(err))
-#toyDistanceClassifier()
-
-#Now train with all the images
+    if verbose > 0:
+        print('Precision: {}'.format(tp/(tp+fp)))
+        print('Recall: {}'.format(tp/(tp+fn)))
+        print('Accuracy: {}'.format(acc))
+        print('Error: {}'.format(err))
+    return (tp/(tp+fp), tp/(tp+fn), acc, err)
 
 def loadAllImages(root):
     '''Note, specifically for loading in images in the dir structure of the yale faces dataset'''
@@ -187,11 +96,13 @@ def loadAllImages(root):
         pickle.dump((imageset, labels), open('im.cache', 'wb'))
     return (imageset, labels)
 
+#TODO replace this with the class for loading in the images
 images, labels = loadAllImages(r'C:/Users/cdilg/Documents/NEU/DS4400/Project/croppedyale/')
 
-splitter = sklearn.model_selection.StratifiedKFold(10)
 
+splitter = sklearn.model_selection.StratifiedKFold(3)
 
+results = []
 i = 0
 for train, test in splitter.split(images, labels):
     i += 1
@@ -200,25 +111,27 @@ for train, test in splitter.split(images, labels):
     train_x = images.iloc[train,:]
     train_y = labels[train]
 
-    #mean_face = np.mean(train_x, axis=0)
-    rpca = PCA(100, svd_solver='randomized')
-    rpca.fit(train_x)
-    pca_train_x = rpca.transform(train_x)
+    knn = KNeighborsClassifier(5)
+    lda = LinearDiscriminantAnalysis(solver='svd')
+    svc = SVC()
+    #eigenFaceView(self._rpca)
+    #projectFaceView(train_x.iloc[50,:], self._rpca)
 
-    #model = KNeighborsClassifier(5)
-    #model = LinearDiscriminantAnalysis(solver='svd')
-    #eigenFaceView(rpca)
-    #projectFaceView(train_x.iloc[50,:], rpca)
-    model = SVC(kernel = 'rbf', class_weight='balanced')
-    model.fit(pca_train_x, train_y)
+    efc = eigenfaces.EigenFaceClassifier(model = lda)
+    efc.fit(train_x, train_y)
 
     test_x = images.iloc[test,:]
-    pca_test_x = rpca.transform(test_x)
+    
     test_y = labels[test]
-    predicted_y = model.predict(pca_test_x)
+    predicted_y = efc.predict(test_x)
 
-    print(sklearn.metrics.accuracy_score(test_y, predicted_y))
+    metrics = sklearn.metrics.classification_report(test_y, predicted_y)
 
+
+    results.append(metrics)
+    print(metrics)
+
+#pd.DataFrame(results).to_csv(open('svc_prae.csv', 'w'))
 
 
 
--- Project/eigenfaces.py
+++ Project/eigenfaces.py
@@ -1,69 +1,165 @@
 import numpy as np
+import os
+import cv2
+
+#Note that this code has been written for this project:
+
+from sklearn.metrics import confusion_matrix
+import sklearn.cross_validation
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+import scipy
+import scipy.stats
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import sklearn.model_selection
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
+
+import pickle
+
+class EigenFacesLoader(object):
+    def __init__(self, image_x, image_y, cache = ''):
+        
+        self._image_x = image_x
+        self._image_y = image_y
+        self._cache = cache
+
+    def resizeImage(self, image):
+        return cv2.resize(image, (image_x, image_y), 0, 0, cv2.INTER_AREA)
 
 
-class CustomPCA():
-    def __init__(self, m):
-        self.m = m
+    def loadSomeImages(root, extension='.pgm'):
+        images = []
+        for path, subdirs, files in os.walk(root):
+            for name in files:
+                if name[-len(extension):] == extension:
+                    image = cv2.imread(os.path.join(path, name))
+                    image = self.resizeImage(image)
+                    images.append(image)
+        return np.array([image[:, :, 0].flatten() for image in images])
 
-    def disp(self, u):
-        plt.scatter(u[:, 0], u[:, 1], label='data')
+    def getEigenFaces(self):
+        return self._pca.components_.reshape((len(pca.components_), image_x, image_y))
 
-        plt.xlabel('x label')
-        plt.ylabel('y label')
+    def loadImages(self):
+        '''Note, specifically for loading in images in the dir structure of the yale faces dataset'''
+        root = self._cache
+        try:
+            if(verbose > 1):
+                print('Loading images')
+            imageset, labels = pickle.load(open('im.cache', 'rb'))
 
-        plt.title("Simple Plot")
+        except FileNotFoundError:
+            if (verbose > 1):
+                print('Falling back to loading manually')
+            imageset = pd.DataFrame()
+            labels = []
+            for path, subdirs, files in os.walk(root):
+                for j, subdir in enumerate(subdirs):
+                    # Create a dataframe with the images in it
+                    images = pd.DataFrame(loadSomeImages(root + subdir))
+                    # Add a label column, with the name of the subdir
+                    labels.extend([j]*len(images))
+                    # Append this to a master DataFrame
+                    imageset = imageset.append(images)
 
-        plt.legend()
+                labels = np.array(labels)
+                break
+            pickle.dump((imageset, labels), open('im.cache', 'wb'))
+        self._images = imageset
+        self._labels = labels
+        return (imageset, labels)
 
+class EigenFaceClassifier(object):
+    def __init__(self, n_eigenfaces = 100, model = KNeighborsClassifier(5), size = 32):
+        self._n_eigenfaces = n_eigenfaces
+        self._model = model
+        self._image_x = self._image_y = size
+        
+
+    def fit(self, x, y):
+        '''
+        x is a list of images
+        y is labels or names
+        
+        Images should already be scaled appropriately, for doing this please use EigenFacesLoader
+        which also has good caching
+        '''
+        train_x = x
+        train_y = y
+        self._rpca = PCA(self._n_eigenfaces, svd_solver='randomized')
+        self._rpca.fit(train_x)
+        pca_train_x = self._rpca.transform(train_x)
+
+        self._model.fit(pca_train_x, train_y)
+       
+
+    def predict(self, x):
+        '''Where x is a list of images'''
+        return self._model.predict(self._rpca.transform(x))
+
+    def pltImage(self, image):
+        plt.imshow(np.resize(image, (self._image_x, self._image_y)), cmap=plt.cm.gray)
         plt.show()
-
-    def fit(self, gamma, verbose = 0):
-        self.dims = gamma.shape[1]
-        '''Gamma is a 2d array, of (m x n) with m training examples and n dimensions'''
         
+    def showFace(self, n = 0):
+        '''Default to show the principal axis of maximal variance'''
+        eigFaces = self._rpca.components_.reshape(
+            (len(self._rpca.components_), image_x, image_y))
+        self._pltImage(eigFaces[n])
 
-        phi = np.mean(gamma, axis=0)
-        self.phi = phi                      # keep the average image
-        psi = gamma - phi
-        w, v = np.linalg.eig(np.matmul(psi.T, psi))
-        sortindex = np.argsort(-w)
-
-        self.components = v[:, sortindex]
-        v = self.components[:, :self.m]
-
-        w = w[sortindex][:self.m]
-
-        u = np.matmul(psi, v)
-        #normalize u
-        norm = np.linalg.norm(u, axis=0)
-        u = u / norm
+class EuclideanFaceClassifier(object):
+    '''Will tell us if the thing is either a face or not, binary'''
+    def __init__(self, n_eigenfaces = 100, cutoff = 41000, ):
+        '''
+        Distance metric chosen by hand to initialise to this value
         
-        self.w = w
-        self.u = u
-        self.v = v
+        TODO: Actually choose distance metric using something like LDA
+        to pick a cutoff point
+        '''
+        self._cutoff = cutoff
+        self._n_eigenfaces = n_eigenfaces
 
-        if verbose == 1:
-            print('psi')
-            print(psi.shape)
-            print('v')
-            print(v.shape)
-            #quickly sort the eigenvalues and then reshuffle the eigenvectors accordingly
-            print("argsorting:")
-            print(w)
-            print(np.argsort(-w))
-            
+    def faceSolver(self, face):
+        '''
+        Warning - code is probably dead. 
+        Do not use anything that normalises faces as pca is smart and already does this for us
         
-        return u
-    def transform(self, image):
-        '''Takes some points and returns the relevant PCA components'''
-        pass
-    def getComponents(self):
-        return self.u
+        :O
 
-    def projectFace(self, image):
-        #TODO add in some errors for mismatched dimensions
-        normalised_image = image - self.phi
-        weights = np.dot(self.u.T, normalised_image)
-        newImage = np.dot(self.u[:, :self.m], weights[:, :self.m])
-        return newImage
+        '''
+        pca_face = self._rpca.transform(face.reshape(1, -1))
+
+        #this step is rather important - we actually obtain the orig face back, but it's actually
+        #just a linear combination of eigenvectors
+
+        projected_face = np.matmul(pca_face, self._rpca.components_)
+
+        #NOTE confusion around how to normalise new inputs, do we divide by the average of a class,
+        # or all classes, or by the mean of the data itself?
+        diff = face - projected_face
+
+        return np.linalg.norm(diff)
+
+    def calculateDistance(self, imageset):
+        #for training data
+        distances = np.apply_along_axis(self.faceSolver, axis=1, arr=imageset)
+        #print(distances)
+        print(scipy.stats.describe(distances)) 
+        print(scipy.stats.tstd(distances)) 
+        #for new images
+        return distances
+
+
+    def fit(self, x, y = None, verbose = 2):
+        self._rpca = PCA(self._n_eigenfaces, svd_solver='randomized')
+        self._rpca.fit(x)
+
+        print('Testing images distance metric')
+        self._train_distances = self.calculateDistance(x)
+    
+    def predict(self, x):
+        return self.calculateDistance(x) < self._cutoff
+
